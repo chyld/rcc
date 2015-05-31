55ddd2caff93d28bc97967b764af5ec5d3878dbc
Chyld Medford <chyld.medford@gmail.com>
2015-05-31 01:38:41 -0700

    initialize

--- lib/cuboid.rb
+++ lib/cuboid.rb
@@ -1,17 +1,35 @@
-
 class Cuboid
-  
-  #BEGIN public methods that should be your starting point
 
-  def move_to!(x, y, z)
-  end
-  
-  def vertices
-  end
-  
-  #returns true if the two cuboids intersect each other.  False otherwise.
-  def intersects?(other)
+  #
+  # PUBLIC METHODS
+  #
+
+  def initialize(origin, volume)
+    raise(ArgumentError, 'origin is invalid') unless origin? origin
+    raise(ArgumentError, 'volume is invalid') unless volume? volume
+
+    @origin = origin
+    @volume = volume
   end
 
-  #END public methods that should be your starting point  
+  #
+  # PRIVATE METHODS
+  #
+
+  private
+
+  def origin?(origin)
+    hash?(origin, [:x, :y, :z])
+  end
+
+  def volume?(volume)
+    hash?(volume, [:width, :height, :length])
+  end
+
+  def hash?(hash, keys)
+    hash.is_a?(Hash) && keys.all? do |key|
+      hash.key?(key) && hash[key].is_a?(Fixnum) && hash[key] >= 0
+    end
+  end
+
 end
--- spec/cuboid_spec.rb
+++ spec/cuboid_spec.rb
@@ -1,17 +1,45 @@
 require 'cuboid'
 
-#This test is incomplete and, in fact, won't even run without errors.  
-#  Do whatever you need to do to make it work and please add your own test cases for as many
-#  methods as you feel need coverage
 describe Cuboid do
- 
-  describe "move_to" do
-    it "changes the origin in the simple happy case" do
-      expect(subject.move_to!(1,2,3)).to be true
+
+  subject do
+    origin = {x: 4, y: 5, z: 6}
+    volume = {width: 2, height: 3, length: 4}
+    Cuboid.new(origin, volume)
+  end
+
+  describe "new" do
+    it "creates a Cuboid" do
+      expect(subject).to be_a Cuboid
     end
-  end    
-  
-  describe "intersects?" do
+
+    it "fails to create a Cuboid - not enough arguments" do
+      expect { Cuboid.new }.to raise_error ArgumentError
+    end
+
+    it "fails to create a Cuboid - origin missing keys" do
+      origin = {}
+      volume = {width: 0, height: 0, length: 0}
+      expect { Cuboid.new(origin, volume) }.to raise_error ArgumentError
+    end
+
+    it "fails to create a Cuboid - volume missing keys" do
+      origin = {x: 0, y: 0, z: 0}
+      volume = {}
+      expect { Cuboid.new(origin, volume) }.to raise_error ArgumentError
+    end
+
+    it "fails to create a Cuboid - origin key is negative" do
+      origin = {x: 0, y: 0, z: -1}
+      volume = {width: 0, height: 0, length: 0}
+      expect { Cuboid.new(origin, volume) }.to raise_error ArgumentError
+    end
+
+    it "fails to create a Cuboid - origin key is a float" do
+      origin = {x: 0, y: 0, z: 1.1}
+      volume = {width: 0, height: 0, length: 0}
+      expect { Cuboid.new(origin, volume) }.to raise_error ArgumentError
+    end
   end
 
 end
